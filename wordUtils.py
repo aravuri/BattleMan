bfc608363046fe2ff982f047feefdd2a6459e1c4
Agastya Ravuri <aggu.ravuri.6@gmail.com>
2024-11-01 19:38:06 -0700

    marginal
    marginal distribution yay

--- wordUtils.py
+++ wordUtils.py
@@ -13,4 +13,21 @@ def topPDFUniform(wordLength, cutoff=10000):
     arr = top(wordLength, cutoff);
     return dict(map(lambda word: (word, 1.0/len(arr)), arr))
 
-print(topPDFUniform(4))
+def marginalPDFchar(wordPdf, c):
+    ret = {}
+    for word, prob in pdf.items():
+        queryResult = re.sub(f'[^{c}]', "_", word)
+        if queryResult in ret:
+            ret[queryResult] += prob
+        else:
+            ret[queryResult] = prob
+    return ret
+
+def printPDF(pdf):
+    for k,v in sorted(pdf.items(), key=lambda p:p[1], reverse=True):
+        print(k,v)
+
+
+pdf = topPDFUniform(7)
+marginalPDF = marginalPDFchar(pdf, 'a')
+printPDF(marginalPDF)
