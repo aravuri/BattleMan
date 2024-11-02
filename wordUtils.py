4e2817fc97622941d29dcf3d5985dec6061bd733
Agastya Ravuri <aggu.ravuri.6@gmail.com>
2024-11-01 17:52:24 -0700

    Update wordUtils.py

--- wordUtils.py
+++ wordUtils.py
@@ -1,9 +1,16 @@
 import wordfreq
+from functools import partial
 import re
 
 def isValid(word, length):
     return re.search("^[a-z]*$", word) and len(word) == length
 
-def top(cutoff, wordLength):
+def top(wordLength, cutoff=10000):
     words = wordfreq.top_n_list('en', cutoff, wordlist='best')
-    return list(filter(lambda w: isValid(w, wordLength), words));
+    return list(filter(partial(isValid, length=wordLength), words));
+
+def topPDFUniform(wordLength, cutoff=10000):
+    arr = top(wordLength, cutoff);
+    return dict(map(lambda word: (word, 1.0/len(arr)), arr))
+
+print(topPDFUniform(4))
